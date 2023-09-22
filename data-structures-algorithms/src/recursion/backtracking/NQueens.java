fa6459e8f5b31ebd30cd8355a9a979b0a482893c
Anuj <asanujsharma05@gmail.com>
2023-09-22 20:46:28 +0530

    backtracking questions

---
+++ data-structures-algorithms/src/recursion/backtracking/NQueens.java
@@ -0,0 +1,73 @@
+package recursion.backtracking;
+
+public class NQueens {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        int possiblePositions = queens(board, 0);
+        System.out.println(possiblePositions);
+    }
+
+    static int queens(boolean[][] board, int row) {
+
+        //end of board is reached
+        if(row == board.length) {
+            printBoard(board);
+            System.out.println();
+            return 1;
+        }
+
+        int count = 0;
+
+        //check all the possible col in that row
+        for(int col=0; col<board.length; col++) {
+            if(isValid(board, row, col)) {
+                //place the queen
+                board[row][col] = true;
+
+                //Current row queen is place, now check next row
+                count+=queens(board, row+1);
+
+                //backtrack
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    static boolean isValid(boolean[][] board, int row, int col) {
+
+        //check vertical boxes
+        for(int i=0; i<row; i++) {
+            if(board[i][col])
+                return false;
+        }
+
+        //check left diagonal
+        int maxLeft = Math.min(row, col);
+        for(int i=1; i<=maxLeft; i++) {
+            if(board[row-i][col-i])
+                return false;
+        }
+
+        //check right diagonal
+        int maxRight = Math.min(row, board.length-1-col);
+        for(int i=1; i<=maxRight; i++) {
+            if(board[row-i][col+i])
+                return false;
+        }
+        return true;
+    }
+
+    static void printBoard(boolean[][] board) {
+        for(boolean[] row: board) {
+            for(boolean col: row) {
+                if(col)
+                    System.out.print("Q ");
+                 else
+                    System.out.print("X ");
+            }
+            System.out.println();
+        }
+    }
+}
--- data-structures-algorithms/src/recursion/backtracking/PathAllDirections.java
+++ data-structures-algorithms/src/recursion/backtracking/PathAllDirections.java
@@ -1,5 +1,7 @@
 package recursion.backtracking;
 
+import java.util.Arrays;
+
 /**
  * Left/Right/Up/Down
  */
@@ -10,7 +12,11 @@ public class PathAllDirections {
                 {true, true, true},
                 {true, true, true}
         };
-        paths("", maze, 0, 0);
+
+        int[][] path = new int[3][3];
+
+//        paths("", maze, 0, 0);
+        pathsPrint("", maze, 0,0, path, 1);
     }
 
     static void paths(String p, boolean[][] maze, int r, int c) {
@@ -43,6 +49,50 @@ public class PathAllDirections {
 
         maze[r][c] = true;
     }
+
+    static void pathsPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+        int rowLen = maze.length;
+        int colLen = maze[0].length;
+
+        if(r==rowLen-1 && c == colLen-1) {
+            path[r][c] = step;
+            for(int[] arr: path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if(!maze[r][c]) {
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        step = step + 1;
+
+        if(r < rowLen-1) {
+            pathsPrint(p + 'D', maze, r+1, c, path, step);
+        }
+        if(c < colLen-1) {
+            pathsPrint(p + 'R', maze, r, c+1, path, step);
+        }
+        if(r>0) {
+            pathsPrint(p + 'U', maze, r-1, c, path, step);
+        }
+        if(c>0) {
+            pathsPrint(p + 'L', maze, r, c-1, path, step);
+        }
+
+        path[r][c] = 0;
+        maze[r][c] = true;
+    }
+
+    /**
+     * Initial solution thought
+     */
     static void paths1(String p, boolean[][] maze, int r, int c) {
         int rowLen = maze.length;
         int colLen = maze[0].length;
